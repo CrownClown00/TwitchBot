dea21e6195272f37c654c2a3c062d9fd7ba67113
CrownClown00 <rossca.thompson@gmail.com>
2016-06-28 20:03:24 -0400

    updates

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    var url="https://api.twitch.tv/kraken/streams?channel=nightblue3,voyboy,wingsofdeath?callback=?";
+    var url="https://api.twitch.tv/kraken/streams?channel=nightblue3,voyboy,wingsofdeath";
     var callback=function(response){
         var voyboy =$("#VoyBoy");
         var nightblue3;
         var wingsofdeath;
         voyboy.append("<p>"+response.streams[2].game+"</p>");
+        console.log(response);
     };
     $.getJSON(url,callback);
     
